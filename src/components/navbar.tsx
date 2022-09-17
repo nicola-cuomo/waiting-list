0c985ad8ad9884600250eb4b3d01c4805c982381
Nicola Cuomo <nicola.cuomo@relatech.com>
2022-09-17 16:08:48 +0200

    change icon size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar({ children }: NavbarProps) {
         <nav className="bg-white px-2 sm:px-4 dark:bg-gray-900 sticky w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://flowbite.com/" className="flex items-center">
-                    <Image width={30} height={30} src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                    <Image width={60} height={60} src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                 </a>
                 <div className="flex md:order-2">
